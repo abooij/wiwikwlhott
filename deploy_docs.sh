9129caf0069a53615ab68df0eddc3e026331801a
Auke Booij <auke@tulcod.com>
2017-12-17 18:35:20 +0000

    Attempt to run the build on travis

---
+++ deploy_docs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+
+# export REMOTE_URL=$(git remote get-url origin)
+
+REPO="git-gh-pages"
+
+git init $REPO
+
+cp -a _build/html/* $REPO
+
+pushd $REPO
+
+export MESSAGE="Autoupdate coqdoc documentation from travis
+
+Generated with 'make html'"
+
+git add .
+git commit -m "$MESSAGE"
+
+git remote add origin git@github.com:abooij/wiwikwlhott # $REMOTE_URL
+
+eval $(ssh-agent -s)
+
+set +x
+echo "Loading SSH private key..."
+echo "$SSH_PRIVATE_KEY" | base64 -d | ssh-add -
+set -x
+
+git push -f origin HEAD:gh-pages
+
+popd
+
+set +x
